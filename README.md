6142116e149c9a191a30bb8e884ff4711e6b4ba5
Adam Chambers <adammchambers@gmail.com>
2017-02-13 10:12:40 -0500

    Initial project commit. Codebase copied from NEJS presentation, set up with RevealJS and CSS/JS build system, ready for presentation dev

---
+++ README.md
@@ -0,0 +1,10 @@
+# 🤓 WebDev for Marketers 🤓
+
+A collection of WebDev tips and tricks for marketers
+
+**Get Started**
+
+	$ npm install
+	$ npm start
+
+**Start**: [http://localhost:8080](http://localhost:8080)
---
+++ js/app.js
@@ -0,0 +1,9 @@
+const Reveal = require('reveal.js');
+
+Reveal.initialize({
+	history: true,
+    dependencies: [
+        // Syntax highlight for <code> elements
+        { src: '/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+    ]
+});
